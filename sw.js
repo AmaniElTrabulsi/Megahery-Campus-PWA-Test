50807b6cba1397bac79c90f8aad88f812e4f3898
amani <amani.eltrabulsi@web.de>
2025-10-27 23:44:46 +0200

    changed the index, added a game, added image

--- sw.js
+++ sw.js
@@ -1,47 +1,29 @@
-const CACHE_NAME = 'megaherz-pwa-v1';
-const FILES_TO_CACHE = [
+const CACHE_NAME = 'megaherz-campus-v3';
+const urlsToCache = [
   '/',
-  '/index.html?v=1',
-  '/manifest.json?v=1',
-  '/images/background.jpg?v=1',
-  '/images/treasure-map.png?v=1',
-  '/images/character.png?v=1',
-  '/docs/sample1.pdf?v=1',
-  '/docs/sample2.pdf?v=1'
+  '/index.html',
+  '/script.js',
+  '/manifest.json',
+  '/images/background.jpg',
+  '/images/map-bg.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Pre-caching offline assets');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('Removing old cache', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
